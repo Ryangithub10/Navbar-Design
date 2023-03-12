9450726fe2b0106856e149494fdf5af8712ef8ed
Muhammad Febrian <technogamers543@gmail.com>
2023-03-12 16:48:56 +0800

    make all elements become semi-black when sidebar button clicked

--- sidebar/js/script.js
+++ sidebar/js/script.js
@@ -8,14 +8,17 @@ const toggleClass = (element, cls) => {
 hamburgerBtns.forEach((btn) => {
   btn.onclick = () => {
     toggleClass(aside, "show-content-aside");
+    toggleClass(aside, "dark-effect");
   };
 });
 
 document.onclick = function (event) {
   const clickWidth = event.clientX;
-  const isOutSideClicked = clickWidth < 200 && event.target.tagName !== "ASIDE";
+  const isInSidebarArea = clickWidth > 200;
+  const isSidebarClick = event.target.tagName === "ASIDE";
 
-  if (isOutSideClicked) {
+  if (!isInSidebarArea && !isSidebarClick) {
     aside.classList.remove("show-content-aside");
+    aside.classList.remove("dark-effect");
   }
 };
